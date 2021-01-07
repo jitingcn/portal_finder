df31838da498cdada2badc7ceb897cc69f89cd43
Jiting <jiting@jtcat.com>
2021-01-07 10:55:58 +0800

    tweak style, remove global purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,5 @@
 /* eslint-disable global-require,import/no-extraneous-dependencies,no-unused-vars */
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./app/javascript/stylesheets/**/*.*css",
-    "./app/javascript/controllers/*.js",
-    "./app/views/**/*.*",
-    "./app/**/*.*",
-    "./app/helpers/**/*.*",
-  ],
-  whitelistPatterns: [/leaflet/, /marker/, /notyf/],
-  // This is the function used to extract class names from your templates
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
 
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-    return broadMatches.concat(innerMatches);
-  },
-});
 module.exports = {
   plugins: [
     require("postcss-import"),
@@ -33,6 +13,5 @@ module.exports = {
       },
       stage: 3
     }),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ]
 }
